58c83cc996f2849dc2537c40cc588b7c9f7e2efb
Noah Kovacs <kovacsn66@gmail.com>
2020-06-20 19:19:48 -0400

    fix: add execution role arn to task definition
    Signed-off-by: Noah Kovacs <kovacsn66@gmail.com>

--- tf/modules/ECS/task-definition.tf
+++ tf/modules/ECS/task-definition.tf
@@ -1,4 +1,5 @@
 resource "aws_ecs_task_definition" "primary" {
+  execution_role_arn = data.aws_iam_role.execution-role.arn
   container_definitions = file("${path.module}/container.json")
   family = "noahkovacs-me"
   memory = "64"
