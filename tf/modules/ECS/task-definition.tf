1a64ee2409154b5ca6976ee5d59b0d5170885cb4
Noah Kovacs <kovacsn66@gmail.com>
2020-06-20 19:46:37 -0400

    infra: update task size to meet minimum cpu, add more RAM
    Signed-off-by: Noah Kovacs <kovacsn66@gmail.com>

--- tf/modules/ECS/task-definition.tf
+++ tf/modules/ECS/task-definition.tf
@@ -2,8 +2,8 @@ resource "aws_ecs_task_definition" "primary" {
   execution_role_arn = data.aws_iam_role.execution-role.arn
   container_definitions = file("${path.module}/container.json")
   family = "noahkovacs-me"
-  memory = "64"
-  cpu = "64"
+  memory = "128"
+  cpu = "128"
 }
 
 data "aws_iam_role" "execution-role" {
